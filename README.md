612dd0c2ada1f0d11406ba5fff8577cfd695e64b
Oscar Wellner <o.wellner@telfort.nl>
2020-09-18 16:16:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# MAD - Level 4: Task 1
+
+This Mobile Application is developed as part of the introduction to the Mobile Application Development at HvA.
+The application is used as an introduction to using local storage to make the data persisted.
+
+
---
+++ app/src/main/java/com/nonamer777/madlevel4task1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nonamer777.madlevel4task1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
